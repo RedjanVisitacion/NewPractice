cf5c2d3e34486efe023ba48ab1e463b0a8d171d8
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-08-19 22:43:45 +0500

    UPDATED

---
+++ variables.dart
@@ -0,0 +1,12 @@
+void main(){
+  String name = 'Redjan';
+  bool male = true;
+  int age = 20;
+  List<String> hobby = ['Coding', 'Photography', 'Videography'];
+  Map<String, String> MyInfo = {
+    'location':'Purok 4, Mobod Oroquieta City'
+  };
+
+  print('My name is $name, and I\'m male!! thats $male, Age of $age, and my hobby is ${hobby[0]}, Leave in ${MyInfo['location']}');
+  
+}
