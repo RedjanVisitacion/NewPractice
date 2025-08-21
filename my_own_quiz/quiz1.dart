75061938f8fc3ae41145ce07ec74795601f06cc4
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-08-21 12:05:51 +0500

    UPDATED

--- my_own_quiz/quiz1.dart
+++ my_own_quiz/quiz1.dart
@@ -12,22 +12,10 @@ void main(){
   print(myDisplay(num1, num2));
 }
 
-mySum(int num1, int num2){
-  return num1 + num2;
-}
+mySum(int num1, int num2) => num1 + num2;
+myDifference(int num1, int num2) => num1 - num2;
+myProduct(int num1, int num2) => num1 * num2;
+myQuotient(int num1, int num2) => num1 / num2;
 
-myDifference(int num1, int num2){
-  return num1 - num2;
-}
+myDisplay(int num1, int num2) => '\n Sum: ${mySum(num1,num2)}\n Difference: ${myDifference(num1, num2)}\n Product: ${myProduct(num1, num2)}\n Quotient: ${myQuotient(num1, num2)}\n';
 
-myProduct(int num1, int num2){
-  return num1 * num2;
-}
-
-myQuotient(int num1, int num2){
-  return num1 / num2;
-}
-
-myDisplay(int num1, int num2){
-  return '\n Sum: ${mySum(num1,num2)}\n Difference: ${myDifference(num1, num2)}\n Prodoct: ${myProduct(num1, num2)}\n Quotient: ${myQuotient(num1, num2)}\n';
-}
