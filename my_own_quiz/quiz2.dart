ace188742dd1e28aa95965687973e8b87fd9993d
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-08-21 12:18:56 +0500

    UPDATED

--- my_own_quiz/quiz2.dart
+++ my_own_quiz/quiz2.dart
@@ -1,9 +1,8 @@
 import 'dart:io';
-void main(){
-  int num;
- 
+void main(){ 
+  
   stdout.write('Enter a number: ');
-  num = int.parse(stdin.readLineSync()!);
+  int num = int.parse(stdin.readLineSync()!);
 
   num % 2 == 0 ? print('$num is even number..') : num % 2 == 1? print('$num is odd number..') : print('$num invalid number..'); 
 }
