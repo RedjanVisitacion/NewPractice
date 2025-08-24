ebeb145a45edc34407fe0b997d5110067827a79d
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-08-24 18:29:32 +0800

    UPDATED

--- my_own_quiz/quiz6.dart
+++ my_own_quiz/quiz6.dart
@@ -1,4 +1,40 @@
 import 'dart:io';
-void main(){
 
+void main() {
+  int num1, num2;
+  String? op;
+
+  stdout.write('Enter first number: ');
+  num1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Enter second number: ');
+  num2 = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Select an operator (+, - , * , / ): ');
+  op = stdin.readLineSync();
+
+  // Pass the actual operator (op!) to the function
+  print(myProcess(num1, num2, op!));
+}
+
+String myProcess(int num1, int num2, String op) {
+  String result;
+
+  if (op == '+') {
+    result = '$num1 + $num2 = ${num1 + num2}';
+  } else if (op == '-') {
+    result = '$num1 - $num2 = ${num1 - num2}';
+  } else if (op == '*') {
+    result = '$num1 * $num2 = ${num1 * num2}';
+  } else if (op == '/') {
+    if (num2 != 0) {
+      result = '$num1 / $num2 = ${num1 / num2}';
+    } else {
+      result = 'Error: Division by zero!';
+    }
+  } else {
+    result = 'Invalid operator!';
+  }
+
+  return result;
 }
